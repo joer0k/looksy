62cd6f9c01d831fdb0cb668c73aca86f8fe63304
Nikita <ryabokon.niki08@yandex.com>
2026-08-17 13:20:14 +0300

    feat: connect frontend health check to API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get('/')
+async def root():
+    return {'message': 'Hello from backend!'}
+
+
+@app.get('/health')
+async def health():
+    return {'status': 'healthy'}
